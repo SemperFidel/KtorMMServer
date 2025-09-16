f035a301d7e428fcd41831f6d09989ff7df158fb
SemperFidel <zandrekh.40000@gmail.com>
2025-09-16 22:00:13 +0300

    created transaction repository

--- database/src/main/kotlin/domain/repository/TransactionRepository.kt
+++ database/src/main/kotlin/domain/repository/TransactionRepository.kt
@@ -1,4 +1,12 @@
 package domain.repository
 
+import arrow.core.Either
+import kotlinx.coroutines.flow.Flow
+import org.jetbrains.exposed.v1.core.Transaction
+
 interface TransactionRepository {
+    fun getAllTransactions(): Flow<Transaction>
+    suspend fun addTransaction(transaction: Transaction): Either<Throwable, Transaction>
+    suspend fun updateTransaction(transaction: Transaction): Either<Throwable, Transaction>
+    suspend fun deleteTransaction(transaction: Transaction): Either<Throwable, Transaction>
 }
